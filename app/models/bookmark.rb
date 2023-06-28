3a03bb8d463a8d3b8b7dadfce690fabf7168d808
Maxime Moreira <maxime.moreira@gmail.com>
2023-06-28 11:39:43 +0200

    added some validations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,8 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+
+  validates :list_id, presence: true
+  validates :movie_id, presence: true
+  validates :comment, presence: true, length: { min: 6 }
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,6 @@
 class List < ApplicationRecord
-  has_many :movies
+  has_many :movies, through: :bookmarks
   has_many :bookmarks
+
+  validates :name, presence: true, uniqueness: true
 end
